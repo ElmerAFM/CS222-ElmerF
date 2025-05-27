0a93ac90ae7df386826367ce270e58c66baf22b4
ElmerAFM <elmerafm4@gmail.com>
2025-05-27 14:57:19 -0400

    Finished Exercise 6-2

---
+++ Exercise6-2/Exercise02/Rectangle.cs
@@ -0,0 +1,9 @@
+public class Rectangle : Shape
+{
+  public Rectangle() { }
+
+  public Rectangle(double height, double width)
+  { this.height = height; this.width = width; }
+
+  public override double Area { get { return height * width; } }
+}
